aef9603a849b3a99ef78f5f39528e5176147f32e
William  Goudie <w.goudie@outlook.com>
2023-07-15 13:48:34 +0000

    Fixed Keybaord viewing

--- screens/LogIn.js
+++ screens/LogIn.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { ImageBackground, Image, StyleSheet, View, TextInput, Text, TouchableOpacity, Alert } from "react-native";
+import { Keyboard, KeyboardAvoidingView, ImageBackground, Image, StyleSheet, View, TextInput, Text, TouchableOpacity, Alert, Platform } from "react-native";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "../utils/Firebase";
 import { useNavigation } from "@react-navigation/native";
 import { doc, getDoc } from "firebase/firestore";
 import styles from "../styles/Login.styles"
+
 export default function Login() {
   const navigation = useNavigation();
   const [email, setEmail] = useState("");
@@ -28,6 +29,7 @@ export default function Login() {
     if (!validateInput()) {
       return;
     }
+
     signInWithEmailAndPassword(auth, email, password)
       .then(async ({ user }) => {
         const docRef = doc(db, "users", user.uid);
@@ -44,32 +46,35 @@ export default function Login() {
       });
   };
 
-
   return (
-    <ImageBackground style={styles.backgroundImage}>
-      <View style={styles.logoContainer}>
-        <Image source={require('../assets/favicon.png')} style={styles.logo} />
-      </View>
-      <View style={styles.container}>
-        <TextInput style={styles.input} placeholder="Email" value={email} onChangeText={setEmail} placeholderTextColor="#000" />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-          placeholderTextColor="#000"
-        />
-        <Text style={styles.errorText}>{error}</Text>
-        <TouchableOpacity style={styles.button} onPress={login}>
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-        <View style={styles.footerContainer}>
-          <Text>Haven't got an account? <Text style={styles.linkText} onPress={() => navigation.navigate("SignUp")}>SignUp</Text></Text>
-          <Text style={styles.forgotPassword} onPress={() => navigation.navigate("ForgotPassword")}>Forgot password?</Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.backgroundImage}
+    >
+      <ImageBackground style={styles.backgroundImage}>
+        <View style={styles.logoContainer}>
+          <Image source={require('../assets/favicon.png')} style={styles.logo} />
         </View>
-      </View>
-    </ImageBackground>
+        <View style={styles.container}>
+          <TextInput style={styles.input} placeholder="Email" value={email} onChangeText={setEmail} placeholderTextColor="#000" />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            placeholderTextColor="#000"
+          />
+          <Text style={styles.errorText}>{error}</Text>
+          <TouchableOpacity style={styles.button} onPress={login}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+          <View style={styles.footerContainer}>
+            <Text>Haven't got an account? <Text style={styles.linkText} onPress={() => navigation.navigate("SignUp")}>SignUp</Text></Text>
+            <Text style={styles.forgotPassword} onPress={() => navigation.navigate("ForgotPassword")}>Forgot password?</Text>
+          </View>
+        </View>
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 }
-
